d914b46608c9d5f952e0839aedfe0294a45f8c6c
Oguzarc <aracoguz@gmail.com>
2019-10-15 18:27:41 +0300

    HTML CSS lere vue.js eklendi. Bazı güncellemeler yapıldı.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+      <Header/>
+      
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {   
+    Header,
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,467 @@
+<template>
+<div>
+	<div id="hvrenk" class="header">
+			<a href="https://www.hollyhood.com.tr/">Hollyhood</a>
+			<a href="#paramkart">Param Kart</a>
+			<a href="#yorumlar">Yorumlar</a>
+			<a href="#kurumsal">Kurumsal</a>				
+			<a href="#ik">İK</a>
+	</div>
+	<div class="header2">
+		<a><img src="../assets/bynogame-logo-siyah.png" style="width:9em"></a>	 
+		<a>
+			<form class="example" action="Anasayfa.html">
+				<input type="text" placeholder="Search.." name="search">	
+				<button type="submit">ARA</button>
+			</form>
+    </a>
+    <a>
+      <ul class="İkonS" style="list-style: none;"> 
+        <a href="#shopcart">
+            <li><i class="fa fa-user fa-lg"></i>
+            </li>
+            Giriş Yap
+        </a>
+        <a href="#shopcart">
+          <li><i class="fa fa-shopping-cart fa-lg"></i>
+          </li>
+          Sepetim
+        </a>
+        <a href="#shopcart">
+          <li><i class="fa fa-credit-card"></i>
+          </li>
+          Ödeme
+        </a>
+      </ul>
+    </a>
+	</div>	 	
+<div class="navbar">
+  <div class="dropdown">
+    <button class="dropbtn">Oyunlar
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">         
+          <ul style="width: 52;">
+          <li >
+            <a id="menu-ana" href=""> Knight Online</a>
+              <ul id="menu-ana-2">
+                <li>
+                  <a href="">Oyun Parası</a>
+                  <ul id="menu-ana-3">
+                    <li>
+                      <a href=""> Goldbar</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <ul id="menu-ana-2" >
+                  <li>
+                    <a  href="">Item &amp; Skin</a>
+                      <ul id="menu-ana-3">                                                                                                           <li>
+                        <li>
+                        <a href=""> Item, </a>
+                        </li>
+                        <li>
+                          <a href=""> Ring</a>
+                        </li>
+                      </ul>
+                  </li>
+              </ul>
+            <ul id="menu-ana-2" >
+              <li>
+                <a  href="">E-Pin</a>
+                  <ul id="menu-ana-3">
+                    <li>
+                      <a href=""> KO Cash, </a>
+                    </li>
+                    <li>
+                      <a href=""> Premium</a>
+                    </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          </ul>
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>		  
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Knight Online
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>  
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">İtem & Skin
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>	  
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">WAX
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>		  
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Pazar
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>
+          <a href="#">deneme</a>		  
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Destekle
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="row">
+        <div class="column">
+          <h6>Category 1</h6>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> 
+</div>
+</template>
+
+<style>
+
+.header {
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	padding: .75em .75em .1em;	
+	margin-left: auto;
+	margin-right: auto;
+	align-items: center;
+	background-color:white;
+}
+.header a{
+	margin-left:20px;
+	float:left;	
+}
+.header2 {
+	position: relative;
+	max-width: 1500px;
+	width: 100%;
+	margin: 0 auto .75em;
+	display: flex;
+	background-color:white;
+}
+.header2 a{
+	margin-left:40px;
+	float:left;
+	
+}
+* {
+  box-sizing: border-box;
+}
+/* ikonların stili*/
+.İkonS{	 
+	color: #435064;
+	text-align:center;	
+  font-size: 18px;
+  margin-left: 5px;
+}
+ .İkonS a:link,a:visited{
+	color:#435064;
+	text-decoration:none;
+	
+}
+ .İkonS a:hover{
+  color:red;
+  transition: color .2s;
+}
+/* Arama çubuğu üstü linkler */
+ #hvrenk{	
+	color: #435064;
+	text-align:center;
+	font-size:14px;
+	
+}
+ #hvrenk a:link,a:visited{
+	color:#435064;
+	text-decoration:none;
+	
+}
+ #hvrenk a:hover{
+  color:red;
+  transition: color .2s;
+}
+
+/* Arama Çubuğu stili */
+form.example input[type=text] {
+  padding: 10px;
+  font-size: 17px;
+  border: 1px solid grey;
+  margin-left: 100px;
+  width: 520px;
+  background: white;
+  height: 2.5em;
+}
+
+/* ARA butonu stili */
+form.example button { 
+  width: center;
+  margin-left:-4px;
+  padding: 9px;
+  background: #e9ecef;
+  color: #495057;
+  font-size: 17px;
+  border: 1px solid #ced4da;
+  border-left: none; /* çift border olmaması için*/
+  cursor: pointer;
+  
+}
+
+form.example input:hover {
+  border: 1px solid red;
+  transition: color .2s;
+}
+
+/* Clear floats */
+form.example::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+  /* Navbar container */
+.navbar {
+  
+  overflow: hidden;
+  background-color: #ed502f;
+  font-family: Arial;
+  float:left;
+  width:100%;
+}
+
+/* navbar içi linkler */
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font: inherit; /* Important for vertical align on mobile phones */
+  margin:0; /* Important for vertical align on mobile phones */
+  width:150px;
+  margin-left:63px;
+
+}
+
+/* mause gelince arka plan değişikliği */
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: white;
+  color:#ed502f;
+  transition: color .2s;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: white;
+  width: 89%;
+  margin-left:70px;
+  left: 0;
+  z-index: 1;
+}
+
+/* dropdown menu  hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+/* 3 tane eşit sütün için */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+  background-color: white;
+  height: auto;
+}
+
+/* Sütun içi stil */
+.column a {
+  float: left;
+  color: black;
+  padding: 2px;
+  text-decoration: none;
+  display: left;
+  text-align: left;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+} 
+
+#menu-ana{
+ display: block;
+ font-size: 15px;
+ font-weight: 700;
+ margin-bottom: .5em;
+ color: #ed502f ;
+}
+
+#menu-ana-2{
+  min-height: 1px ;
+  display: inline;
+  padding-right: 0;
+  font-size: 14px;
+  font-weight: 600;
+  position: relative;
+  color: #435064 ; 
+}
+#menu-ana-3{
+min-height: 1px ;
+display: inline;
+padding-right: 0;
+font-size: 13px;
+font-weight: 500;
+position: relative;
+margin-right: .4em;
+color: #435064 ;
+}
+
+ul{
+  display: inline;
+  list-style: none;
+}
+
+</style>
