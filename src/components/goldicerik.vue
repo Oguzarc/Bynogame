89e963ac4ef1fb543dbce363215bb2ee5720d3a7
Oguzarc <aracoguz@gmail.com>
2019-11-07 04:03:11 +0300

    gold içerik düzenlendi

--- src/components/goldicerik.vue
+++ src/components/goldicerik.vue
@@ -410,10 +410,240 @@
                 </div>
                 </b-card>
             </b-col>
-        </b-row>    
+        </b-row>
+        <!--tablo--> 
+        <b-row>             
+            <div style="margin: 2em auto;">
+                <b-table striped hover bordered :items="items"></b-table>
+            </div>
+        </b-row>
+        <!--tablo altı içerikler-->
+        <div class="row games_icerik">
+                <div class="">
+                    <div class="col-12 ko-icerik" style="line-height:1.1em !important; padding-right:0!important; padding-left:0!important">
+                        <h2 class="text-center" style="margin-top: 1em; margin-bottom: 1em; padding-bottom: 0.5em; border-bottom: 1px solid #ebebeb;">Knight Online GB Nasıl Alabilirim?</h2>
+                        <p style="text-align: center;">&nbsp;</p><p style="text-align: center;">
+                            <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;"><strong>
+                                <span style="text-decoration: underline;">Teslimat yeri</span></strong>;&nbsp;
+                                <strong>&nbsp;</strong><strong>
+                            <span style="text-decoration: underline;">
+                                <span style="color: #ed502f; text-decoration: underline;">Folk Village</span></span></strong></span></p>
+                                <hr><p style="text-align: center;">
+                                    <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                        <strong>ByNoGame OYUN İÇİ KONUŞMAZ<br></strong>
+                                        <span style="color: #ed502f;">
+                                            <strong>Oyun içi konuşanlar dolandırıcılardır.&nbsp;PM Blocklayın.</strong></span></span></p>
+                                            <p style="font-size: 12px; font-weight: normal; text-align: center;">
+                                                <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                    <strong>ASLA GBnizi kimseye GERİ VERMEYİN.<br></strong><span style="color: #ed502f;">
+                                                        <br></span></span></p>
+                                                        <p style="font-size: 12px; font-weight: normal; text-align: center;">
+                                                            <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">İster Miktar yazarak&nbsp;
+                                                                <button class="button button-blue width-100px sm v-align-middle" name="tur" type="submit" value="satis">Satın Al</button> a&nbsp;
+                                                                </span>tıklayıp istediğiniz adet<strong>*</strong>&nbsp;kadar satın alın.&nbsp;</p><hr>
+                                                                <p style="font-size: 12px; font-weight: normal; text-align: center;"><span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">İsterseniz de&nbsp;</span>
+                                                                <strong style="font-family: Ubuntu, sans-serif; font-size: 10pt;">Miktar</strong>
+                                                                <span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">&nbsp;kısmına&nbsp;</span>
+                                                                <strong style="font-family: Ubuntu, sans-serif; font-size: 10pt;">Almak istediğiniz rakamı yazın ve&nbsp;Liralık&nbsp;</strong>
+                                                                <span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">seçip &nbsp;</span>
+                                                                <button class="button button-blue width-100px sm v-align-middle" name="tur" type="submit" value="satis">Satın Al</button>
+                                                                <span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">&nbsp;a&nbsp; tıklayarak&nbsp;</span>
+                                                                <strong style="font-family: Ubuntu, sans-serif; font-size: 10pt;">PARANIZ KADAR</strong><strong style="font-family: Ubuntu, sans-serif; font-size: 10pt;">*</strong>
+                                                                <span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">&nbsp;alabilirsiniz.</span>&nbsp;</p><p style="text-align: center;">
+                                                                    <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                                        <img src="https://www.bynogame.com/images2/img/112/Image/kurus.png" alt="" width="60" height="106" align="left"></span></p>
+                                                                        <p style="font-size: 12px; font-weight: normal; text-align: center;">&nbsp;</p>
+                                                                        <p style="text-align: center;"><span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">&nbsp;</span>
+                                                                        <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">Paranızı&nbsp;
+                                                                            <span style="color: #ed502f;"><strong>Kuruşu</strong><strong>&nbsp;</strong></span>
+                                                                            <strong>=</strong>&nbsp;<span style="color: #ed502f;">
+                                                                                <strong>Kuruşuna</strong></span><strong>
+                                                                                    <span style="color: #800080;">&nbsp;</span>(En az 50 M)&nbsp;</strong>harcayın.</span></p>
+                                                                                    <p style="font-size: 12px; font-weight: normal; text-align: center;">
+                                                                                        <span style="font-family: Ubuntu, sans-serif; font-size: 10pt;">&nbsp;* En az 50M satın alabilirsiniz.</span></p>
+                                                                                        <p style="font-size: 12px; font-weight: normal; text-align: center;">&nbsp;*50M : 50.000.000 Coinstir. (Noah)&nbsp;</p><hr><p style="text-align: center;">
+                                                                                            <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                                                                <span style="color: #ed502f;"><strong>Teslimat yeri:&nbsp;</strong>
+                                                                                                </span><strong><span style="color: #ed502f;">Folk Village (Bankanın Yanında).</span></strong>&nbsp;&nbsp;
+                                                                                                <span style="color: #ed502f;"><strong>Hazır olmadan</strong></span> sipariş vermeyiniz.</span></p>
+                                                                                                <p style="text-align: center;">
+                                                                                                    <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">Nickinizi BÜYÜK küçük harfe duyarlı yazın.</span></p><hr>
+                                                                                                    <p style="text-align: center;"><span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                                                                        <strong><span style="color: #ed502f;">21 gb den fazla olan teslimatlar</span>:&nbsp;Önce 20 gb</strong>&nbsp;teslim edilir,&nbsp;</span></p>
+                                                                                                        <p style="text-align: center;"><span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">Bankaya koyarsınız veya&nbsp;</span>
+                                                                                                        <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;"><strong style="font-family: sans-serif;">yan çar</strong>la gelirsiniz, 
+                                                                                                        <strong style="font-family: sans-serif;">yan çarın nickini&nbsp;</strong><br></span></p>
+                                                                                                        <p style="text-align: center;"><span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                                                                            <strong> <span style="text-decoration: underline;">canlı destek</span></strong>ten yazabilirsiniz.<br><br>
+                                                                                                            <strong style="font-family: Arial;"><strong style="font-family: Arial;">Şartları okumadan ve hazır olmadan&nbsp;
+                                                                                                                <span style="color: #ff6600;">TAMAM</span>&nbsp;tuşuna&nbsp;<span style="color: #ed502f;">BASMA</span>yınız.</strong><br></strong></span></p>
+                                                                                                                <p class="MsoNormal" style="text-align: center;">
+                                                                                                                    <span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">
+                                                                                                                        <strong style="font-family: Arial;"><span style="color: #ff6600;">TAMAM</span></strong>a bastığınızda&nbsp;
+                                                                                                                        <a style="font-family: ’Microsoft Sans Serif’;" target="_blank"><strong>SÖZLEŞME</strong></a>&nbsp;yi okumuş ve şartları kabul etmiş sayılırsınız.</span></p>
+                        <h2 class="text-center" style="margin-top: 1em; margin-bottom: 1em; padding-bottom: 0.5em; border-bottom: 1px solid #ebebeb;">Pazar Nasıl Kurulur?</h2>
+                        <p style="text-align: center;">
+                            <iframe src="https://www.youtube.com/embed/19K0TiStsvU" allowfullscreen="" width="560" height="315" frameborder="0"></iframe>
+                        </p>
+                        <h2 class="text-center" style="margin-top: 1em; margin-bottom: 1em; padding-bottom: 0.5em; border-bottom: 1px solid #ebebeb;">Knight Online GB Nasıl Satılır?</h2>
+                        <p style="text-align: center;">
+                            <img src="https://images.bynogame.com/images/anlatim/gbnasilsatilir.png" alt="" width="936" height="55"></p><p style="text-align: center;">
+                                <iframe src="https://www.youtube.com/embed/4a2QM4mACNM" width="425" height="350"></iframe></p>
+                                <p style="text-align: center;">1a)&nbsp;Adetle satmak için Miktar&nbsp;Kısmına&nbsp;SAYIyı girip Kırmızı&nbsp;
+                                    <button class="button sm button-green width-100px v-align-middle" name="tur" value="alis">Satış Yap</button>&nbsp;tuşuna tıklayın. (Küsüratlı satabilirsiniz 3.7 adet gibi)</p>
+                                    <p style="text-align: center;">1b) Veya <span style="text-align: center;">Miktar kısmına&nbsp;</span>Kaç paralık satmak istiyorsanız o tutarı&nbsp;<span style="text-align: center;"> 
+                                        <input autocomplete="off" name="miktar2" size="4" type="text" value="">&nbsp;</span>
+                                        <span style="text-align: center;">girin ardından Liralık kutucuğunu işaretleyin
+                                            <input name="tl35409" type="checkbox" value="1"></span>&nbsp;
+                                            <span style="color: #000000;">ve 
+                                                <button class="button sm button-green width-100px v-align-middle" name="tur" value="alis">Satış Yap</button>&nbsp;tuşuna basın.&nbsp;</span></p>
+                                                <p style="text-align: center;">3)&nbsp;<em>Teslimat Uyarıları</em>nı okuyun ve hazır olmadan başlatmayın.</p>
+                                                <p style="text-align: center;">3a)&nbsp;GB Operatörümüz hazır olduğunda:&nbsp;<em>Teslim Edeceğiniz Karakter&nbsp;</em>kısmında çarımızın adı belirecektir.&nbsp;Pazar Kuracağız veya Trade atacağız. (Yüklü gb verecekseniz Örnek: 216 Adet Tümünü birden başlatın ve bankanız dolu gelin, Yan charlarınız için sizi bekleriz.)</p>
+                                                <p style="text-align: center;">&gt;&gt; &nbsp;PM BLOCK: PMniz kapalı olsun.&nbsp;OYUN İÇİnden&nbsp;ASLA&nbsp;kimse ile&nbsp;KONUŞMAyınız. (Dolandırılırsınız)</p>
+                                                <p style="text-align: center;">3b) İşlem tamam.&nbsp;<span style="color: #339966;">Para hesabınızda</span>. Sitemiz size bu şekilde durumu belirten bir uyarı mesajı gönderecektir.</p>
+                                                <p style="text-align: center;">&nbsp;<span style="background-color: #eeeeee;"><span style="color: #ff3b00;">Verdiğiniz SERVER... Adet X Alış fiyatımız = ...... TL Site hesabınıza eklenmiştir. Alışverişinize devam edebilirsiniz. ByNoGame iyi oyunlar diler.</span></span></p>
+                                                <p style="text-align: center;"><span style="color: #339966;">Bu bakiye ile ister alışveriş yapın - İsterseniz paranızı bankanıza çekin.</span></p><h2 style="text-align: center;"><span style="color: #000000;"><strong>Teslimat Noktasına Nasıl Gidilir?</strong></span></h2>
+                                                <p style="text-align: center;"><strong><img src="https://cdn.bynogame.com/test/Folkvillage.jpg" alt="" width="740" height="650"></strong></p><p style="text-align: center;">&nbsp;</p>
+                                                <p style="text-align: center;">Teslimat yerimiz:<strong>&nbsp;Folk Village&nbsp;( Bankanın(Kronil) Yanında )&nbsp;</strong></p>
+                                                <p style="text-align: center;"><img src="https://cdn.bynogame.com/test/gbanlatim1.png" alt="" width="740" height="450"></p>
+                                                <p style="text-align: center;">Elinizde bulunan oyun paralarını bize satarak yerine başka bir server veya sitemizde bulunan herhangi bir başka ürünü satın alabilirsiniz. (&nbsp;TAKAS&nbsp;)&nbsp;
+                                                    <br>Elinizdeki ürünü değerlendirebilir, sıkıldığınız serverdan başka bir servera geçiş yapabilir, oynadığnız serverda kazandığınız oyun paraları ile premiumunuzu alabilirsiniz.&nbsp;</p><hr>
+                                                <p style="text-align: center;"><span style="color: #3399ff;">ByNoGame®&nbsp;</span>&nbsp;İyi Oyunlar Diler.</p>
+                    </div>
+                </div>
+            </div>       
     </div>
 </div>
 </template>
+
+<script>
+export default {
+    data() {
+      return {
+        items: [
+          {
+            Server: 'Sirius',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+          {
+            Server: 'Vega',
+            '400Cash': '0.8',
+            '800Cash': '1.6',
+            '1200Cash': '2.4',
+            '1600Cash': '3.2',
+            '2000Cash': '3.9',
+            '2400Cash': '4.7',
+            '3200Cash': '6.3',
+            Bronze: '1.4',
+            Gold: '3.9',
+            Platinium: '5.5'
+          },
+           {
+            Server: 'Altar',
+            '400Cash': '0.7',
+            '800Cash': '1.3',
+            '1200Cash': '2',
+            '1600Cash': '2.6',
+            '2000Cash': '3.9',
+            '2400Cash': '5.2',
+            '3200Cash': '4.2',
+            Bronze: '1.2',
+            Gold: '3.3',
+            Platinium: '4.6'
+          },
+           {
+            Server: 'Olympia',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+           {
+             Server: 'Ares ',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+           {
+            Server: 'Diez ',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+           {
+            Server: 'Gordion',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+           {
+            Server: 'Rosetta',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+          {
+            Server: 'Destan',
+            '400Cash': '0.6',
+            '800Cash': '1.1',
+            '1200Cash': '1.6',
+            '1600Cash': '2.1',
+            '2000Cash': '2.6',
+            '2400Cash': '3.1',
+            '3200Cash': '4.2',
+            Bronze: '1',
+            Gold: '2.6',
+            Platinium: '3.6'
+          },
+          
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
 .ko-gb-content p {
