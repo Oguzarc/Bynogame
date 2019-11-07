3e47dd8b6fb6557105b0fa32f056b91f4a5f01ba
Oguzarc <aracoguz@gmail.com>
2019-11-07 22:49:13 +0300

    ilk itemin icerik resmi ve sap blok eklendi

---
+++ src/components/ilkitemicerik.vue
@@ -0,0 +1,94 @@
+<template>
+    <div style="margin-top:80px">
+        <b-container fluid>          
+            <b-row>
+                <!--resim kısım-->             
+                <b-col >
+                <div class="bolge2">                  
+                    <img src="https://cdn.bynogame.com/koitem/ilanresima4506329e16f8cdd1ca816e462c8d05f.jpg" >
+                </div>
+                </b-col>
+                <!--satın alma kısmı-->
+                <b-col >
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 item-detail-right-side" style="background-color: #fff;height: 38em; padding: 0">
+                    <div class="row mt-3 mb-3 mr-0 ml-0">
+                        <div class="col  d-flex justify-content-center pl-0 pr-0">
+                             <span  style="margin-left: 1em; font-size: 0.9em">Satıcı: <a ><span style="color: #ed502f;font-size: 1.2em"> pazarbeyefendisi <span style="color: #33b339;">(10 Başarılı)</span> </span></a> </span>
+                                <span style="margin-left: auto; font-size: 0.9em">Tarih: <span style="color: #ed502f;font-size: 0.9em"> Bugün</span> </span>
+                        </div>
+                    </div>
+                    <h6 class=" ml-3 mt-3" style="color: #ed502f">#3232996
+                        - Iron Bow  Reb + 11 </h6>
+                    <div class="row justify-content-center" style="height: 66%; margin: 0;">
+                        <div class="col-12 mt-2 item-detail-info">
+                            <div class="p-2" style="background-color: white; height: 4em; border-bottom: 1px solid #ebebeb;">
+                                <div  style="display: flex; flex-direction: column">
+                                <span  style="font-weight: bold; font-size: 1em; line-height: 1.5em">Human/Karus:</span>
+                                    <span ></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 mt-2 ">
+                            <div class="p-2" style="background-color: white; height: 4em;
+                          border-bottom: 1px solid #ebebeb;">
+                                <div  style="display: flex; flex-direction: column">
+                                <span  style="font-weight: bold; font-size: 1em; line-height: 1.5em">Server:</span>
+
+                                    <span >Altar</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 mt-2 ">
+                            <div class="p-2" style="background-color: white; height: 9em;
+                         overflow: auto;">
+                                <div  style="display: flex; flex-direction: column">
+                                <span  style="font-weight: bold; font-size: 1em; line-height: 1.5em">Açıklama:</span>
+
+                                    <small class="mb-2" style="font-size: 0.75em; color: #ed502f; position: absolute;
+                                 right: 1.4em;">Teslimat Saatleri:
+                                        23:50 - 14:50 </small>
+                                    <div class="d-flex flex-column" style="width: 100%;">
+                                        <div  style="font-size: 0.75em; text-align: initial">Alan Kişiye Hayırlı Olsun  Görseldeki Gibidir</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row flex-column" style="margin: 0; background-color: #ebebeb; height: 22.6%;">
+                        <div class="col d-flex flex-column">
+                            <div class="row ilan-buttons" style="margin: 0;">
+                                <div class=" pl-0 pr-0 d-flex align-items-center">
+                                    <span class=" ml-3" style="font-size: 2em">3200 TL</span>                                   
+                                </div>
+                                <div class="col d-flex justify-content-around ">
+                                    <div style="width: 100%">
+                                        <form >
+                                            <button  class="btn game-item-list-purchase-buttons btn-success item-comment-btn"   type="submit" style="width: 100%;">
+                                                Satın Al
+                                                <span>
+                                        <i class="fa fa-shopping-cart ml-1" style="color: #fff !important;" ></i>
+                                    </span>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </b-col>
+            </b-row>  
+        </b-container>      
+    </div>
+</template>
+
+<style scoped>
+.bolge2{
+    margin-left: 30%
+}
+.bolge2 img{
+    height: 80%;
+    width: 80%;
+}
+
+</style>
