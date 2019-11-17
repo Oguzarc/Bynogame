2cab0f1549476e10b927008e7a34e9f8f001a987
Oguzarc <aracoguz@gmail.com>
2019-11-18 00:32:12 +0300

    üyelik sayfası tasarlandı

---
+++ src/components/uyelikbody.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="section-header mb-20">
+                <div class="bolge">
+                    <h2>Üyelik Sözleşmesi</h2>
+                </div>
+            </div>
+        <div class="section-content">
+            <div class="bolge" style="line-height: 1.2">
+                <p><span style="font-size: 10pt; font-family: Ubuntu, sans-serif;">BYNOGAME® KULLANICI SÖZLEŞMESİ<br>
+                <br>Bu sözleşme, sitemizdeki hizmetlerden yararlanabilmeniz için gerekli kuralları içermektedir.<br>
+                <br>SİTEMİZE ÜYE OLARAK; KULLANICI SÖZLEŞMESİ’Nİ OKUDUĞUNUZU, İÇERİĞİNİ ANLADIĞINIZI VE HÜKÜMLERİNİ KABUL ETTİĞİNİZİ VE ONAYLADIĞINIZI KABUL, BEYAN VE TAAHHÜT ETMİŞ OLUYORSUNUZ.
+                <br>
+                <br>1. Taraflar<br>
+                <br>İşbu sözleşme ve sözleşmeyle atıfta bulunulan ve sözleşmenin ayrılmaz birer parçası olan belgelerden ve eklerden (EK-1 Gizlilik Politikası) oluşan işbu Bynogame® Kullanıcı Sözleşmesi (bundan böyle ’Kullanıcı Sözleşmesi’ olarak anılacaktır.), Bynogame® ile Site’ye üye olan Kullanıcı arasında, Kullanıcı’nın Site’ye üye olması amacıyla ve sözleşmenin ilgili Site’nin bulunduğu elektronik ortamda Kullanıcı tarafından onaylanması anında düzenlenmiştir.
+                <br>Kullanıcı; Site’ye üye olarak Kullanıcı Sözleşmesi’nin tamamını okuduğunu, içeriğini bütünü ile anladığını ve tüm hükümlerini onayladığını kabul, beyan ve taahhüt eder.<br>
+                <br>2. Tanımlar<br>
+                <br>Kullanıcı: Site’ye üye olan ve Site’de sunulan Hizmet’lerden, işbu sözleşmede belirtilen koşullar dahilinde yararlanan gerçek ya da tüzel kişi.
+                <br>Site: www.bynogame.com isimli alan adından ve bu alan adına bağlı alt alan adlarından oluşan web sitesi Bynogame® tarafından, Site içerisinde ortaya konulan ürünler.<br>
+                <br>3. Sözleşmenin Konusu ve Kapsamı<br>
+                <br>Kullanıcı Sözleşmesi’nin konusu, Site’de sunulan Hizmet’lerin, bu Hizmet’lerden yararlanma şartlarının ve tarafların hak ve yükümlülüklerinin tespitidir.
+                <br>Kullanıcı Sözleşmesi’nin kapsamı, işbu sözleşme ve ekleri ile Site içerisinde yer alan kullanıma, üyeliğe ve Hizmet’lere ilişkin olarak Bynogame® tarafından yapılmış olan bilcümle uyarı, yazı ve açıklama gibi beyanlardır. Kullanıcı, Kullanıcı Sözleşmesi’nin hükümlerini kabul etmekle, Site içinde yer alan kullanıma, üyeliğe ve Hizmet’lere ilişkin olarak Bynogame® tarafından açıklanan her türlü beyanı da kabul etmiş olmaktadır. Kullanıcı, bahsi geçen beyanlarda belirtilen her türlü hususa uygun olarak davranacağını kabul, beyan ve taahhüt eder.
+                <br>
+                <br>4. Üyelik ve Hizmet Kullanımı Şartları<br>
+                <br>a) Üyelik, Site’nin ilgili bölümünden, Kullanıcı olmak isteyen kişi tarafından Site’ye üye olmak için gerekli kimlik bilgilerinin gönderilmesi suretiyle kayıt işleminin yaptırılması ve Bynogame® tarafından kayıt işleminin onaylanması ile tamamlanır. Üyelik işlemi tamamlanmadan, işbu sözleşmede tanımlanan Kullanıcı olma hak ve yetkisine sahip olunamaz.<br>
+                <br>b) Site’ye üye olabilmek için, reşit olmak ve işbu sözleşmenin 5.2 maddesi uyarınca Bynogame® tarafından, geçici olarak üyelikten uzaklaştırılmış veya üyelikten süresiz olarak yasaklanmış olmamak gerekmektedir. Reşit olmayan veya yukarıda belirtildiği gibi, işbu sözleşmenin 5.2 maddesi uyarınca, Bynogame® tarafından geçici olarak üyelikten uzaklaştırılmış veya üyelikten süresiz yasaklanmış olan kişilerin Site kayıt işlemlerini tamamlamış olmaları, Site üyesi olmaları sonucunu doğurmayacaktır.</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.bolge {
+    margin-right: auto;
+    margin-left: auto;
+    padding-left: 10%;
+    padding-right: 10%;
+}
+.section-content{
+    font-weight: 600;
+    color:#939393;
+    margin-top: 15px;
+    font-size: 14px;
+}
+
+.section-header {
+
+    text-align: center;
+    padding: 0;
+
+}
+</style>
